ea488d34973fbb440fa87fc936535dff41f871f9
Sam Vilain <sam@vilain.net>
2014-06-22 14:33:25 -0700

    Add ant build files
    Gradle seems slow, and hideously complicated.  Moving back to submodules
    and ant.

---
+++ ext/mkjar.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+(cd android-smart-image-view; ant jar)
+(cd ../libs;
+wget https://github.com/loopj/android-async-http/raw/master/releases/android-async-http-1.4.4.jar)
+(cd commons-io; ant jar)
+(cd commons-lang; ant jar)
+(cd scribe-java; mvn source:jar)
+#(cd jackson-core; mvn source:jar)
+#(cd jackson-databind; mvn source:jar)
+(cd ../libs
+ wget http://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.4.0/jackson-databind-2.4.0.jar
+ wget http://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/2.4.0/jackson-core-2.4.0.jar
+ wget http://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.4.0/jackson-annotations-2.4.0.jar
+ )
+(cd ext/JSONassert; mvn package)
+#(cd ext/org.json-java; mvn package)
+(cd ../libs
+wget -O org.json-20131017.jar http://search.maven.org/remotecontent?filepath=org/codeartisans/org.json/20131017/org.json-20131017.jar )
